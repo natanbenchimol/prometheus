82a5b9a36346b0c00376b62f09a96e7213291452
natanbenchimol <43557172+natanbenchimol@users.noreply.github.com>
2019-04-09 15:23:24 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+from tkinter import Tk, Label, Button
+
+import RPi.GPIO as GPIO
+# GPIO = None
+
+import time
+
+
+class PrometheusMain:
+    def __init__(self, master):
+        self.master = master
+        
+        if GPIO:
+            GPIO.setmode(GPIO.BOARD)
+            
+        master.title("PROMETHEUS | Liquid Propulsion Lab | USC")
+
+        self.label = Label(master, text="Valve Actuators")
+        self.label.pack()
+
+        self.greet_button = Button(master, text="Actuate", command=self.actuate)
+        self.greet_button.pack()
+
+        self.close_button = Button(master, text="Close", command=self.exit_main)
+        self.close_button.pack()
+
+    def actuate(self):
+        if GPIO:
+            GPIO.setup(7, GPIO.OUT)
+            GPIO.output(7, True)
+
+    def exit_main(self):
+        if GPIO:
+            GPIO.cleanup()
+        print("some")
+        self.master.quit()
+
+
+root = Tk()
+my_gui = PrometheusMain(root)
+root.mainloop()
