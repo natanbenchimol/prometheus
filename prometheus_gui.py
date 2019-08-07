ff31b72c8f6eba9374e009ac909f09d37a073e2b
ajvadera <ajvadera@usc.edu>
2019-08-06 17:11:18 -0700

    nvm looks bad

--- prometheus_gui.py
+++ prometheus_gui.py
@@ -92,10 +92,10 @@ class PrometheusGUI:
         self.fire_ops = tk.Button(self.f1, text="Firing Mode", font=(font, 20), bg='#c41e3a', command=lambda: self.switch_2('Fire'))
         self.fire_ops.grid(column=2, row=0, sticky=(N, S, E, W))
 
-        self.abort_ops = tk.Button(self.f1, text="Aborts Values", font=(font, 15), bg='#bc13fe', borderwidth='5', relief='ridge', command=lambda: self.switch_3('aborts'))
-        self.abort_ops.grid(column=4, row=0, sticky=(N+E + S + W), padx=10, pady=10)
-        self.sense_ops = tk.Button(self.f1, text="Readouts", font=(font, 15), bg='#fe019a', borderwidth='5', relief='ridge', command=lambda: self.switch_3('sense'))
-        self.sense_ops.grid(column=3, row=0, sticky=(N + E+ W + S), padx=10, pady=10)
+        self.abort_ops = tk.Button(self.f1, text="Set Aborts Values", font=(font, 15), bg='#bc13fe', borderwidth='5', relief='ridge', command=lambda: self.switch_3('aborts'))
+        self.abort_ops.grid(column=4, row=0, sticky=(N+E + S + W), pady=10)
+        self.sense_ops = tk.Button(self.f1, text="Senor Readouts", font=(font, 15), bg='#fe019a', borderwidth='5', relief='ridge', command=lambda: self.switch_3('sense'))
+        self.sense_ops.grid(column=3, row=0, sticky=(N + E+ W + S), pady=10)
 
 
 # ------------------------------------- frame 4 (set firing parameters) -----------------------------------------------#
