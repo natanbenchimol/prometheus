dfacc38db2515504d14200dde7363f5178bfa943
Sean Lissner <slissner@usc.edu>
2019-08-17 13:41:50 -0700

    Cleaned up sol mgmt class - Sean

--- SolenoidManagerClass.py
+++ SolenoidManagerClass.py
@@ -2,6 +2,7 @@
 # the state of the solenoids in the system
 
 import prometheus_consts as CONST
+from datetime import time
 # import RPi.GPIO as gpio
 
 
@@ -28,32 +29,32 @@ class SolenoidManager:
             "SPRK": int     # All ints need to be replaced with pin numbers
         }
 
-    # Changes a state of a single solenoid
-    # called by state_change and by user on GUI
-    def toggle(self, name):
-        # gpio.output(self.pin_mapping[name], gpio.HIGH) # This code is taken from swapnils snippet
-        self.write_to_log()
-
     # Changes the state of solenoid 'name' to 'state'
     # if sol already in that state then do nothing
+    # DOES NOT RECORD STATE CHANGE IN LOG FILE
     def solenoid_to_state(self, name, state):
         pass
 
+    # Changes the state of solenoid 'name' to 'state'
+    # if sol already in that state then do nothing
+    # RECORDS STATE CHANGE IN LOG FILE
+    def solenoid_to_state_log(self, name, state, logfile):
+        pass
+
     # Makes large change to entire system state
+    # Used during abort scenarios
     def state_change(self, new_state):
         # Loops thru both dicts (easy because keys are the same)
         for name in CONST.SOL_NAMES:
             if new_state[name] != self.curr_state[name]:
                 # gpio.output(self.pin_mapping[name], gpio.HIGH) # This code is taken from swapnils snippet
                 pass
-        self.write_to_log()
 
-    # Function actually responsible for sending electric signal
-    # called by toggle and state change, not user facing
-    def toggle_from_address(self, address):
-        # POTENTIALLY REDUNDANT FUNCTION?
-        pass
+        # Write this info to logfile later, this has to be fast because of abort
 
     # Writes current state to logfile, called every time state changes
-    def write_to_log(self):
-        pass
+    def write_to_log(self, name, state, logfile):
+        print("Sol '" + name + "' state set to '" + str(state) + "' at " + str(time), file=logfile)
+
+    def write_abort_to_log(self, abort, logfile):
+        print("SYSTEM ABORTED: " + abort + " at time " + str(time), file=logfile)
